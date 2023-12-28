d012c7844eeeb498c4dc19105975728e8d06ac8d
chrisalbon <3179476+tradinscd@users.noreply.github.com>
2023-12-27 19:22:25 -0800

    fixed linter errors

--- main.py
+++ main.py
@@ -1,30 +1,32 @@
-# Reads data from a CSV file containing O'Reilly book annotations, and then
-# writes the annotations in Markdown format to an output file.
+"""Reads data from a CSV file containing O'Reilly book annotations, and then
+writes the annotations in Markdown format to an output file.
+"""
 
+import argparse
 import csv
-from bs4 import BeautifulSoup
 
 
 def read_csv(file_name):
-    annotations = []
+    """Reads a CSV file and returns a list of dictionaries."""
+    annotation_list = []
     with open(file_name, "r", newline="", encoding="utf-8") as csvfile:
         reader = csv.DictReader(csvfile)
         for row in reader:
-            annotations.append(row)
-    return annotations
+            annotation_list.append(row)
+    return annotation_list
 
 
 def write_markdown(annotations, output_file="oreilly-annotations.md"):
+    """Writes a list of annotations to a Markdown file."""
     with open(output_file, "w", encoding="utf-8") as md_file:
         for annotation in annotations:
-            print(annotation)
             note = annotation["Personal Note"].strip()
             highlight = annotation["Highlight"].strip()
             book_title = annotation["Book Title"].strip()
             chapter_title = annotation["Chapter Title"].strip()
             book_link = annotation["Book URL"].strip()
             md_file.write(f"## {note}\n")
-            md_file.write(f"\n")
+            md_file.write("\n")
             md_file.write(
                 f'> "{highlight}" ([{book_title}, {chapter_title}]({book_link}))\n\n'
             )
@@ -33,9 +35,9 @@ def write_markdown(annotations, output_file="oreilly-annotations.md"):
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Process some integers.")
     parser.add_argument(
-        "--csv_file", default="oreilly-annotations.csv", help="CSV file to process"
+        "--csv_file", default="oreilly-annotations.csv", help="CSV file"
     )
     args = parser.parse_args()
 
-    annotations = read_csv(args.csv_file)
-    write_markdown(annotations)
+    annotations_file = read_csv(args.csv_file)
+    write_markdown(annotations_file)
